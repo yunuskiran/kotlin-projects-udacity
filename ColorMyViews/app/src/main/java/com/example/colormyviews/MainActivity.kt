c74de5a6f16cd2f680ea5b7223929a5cdf4f0820
yunuskiran <yunuskiran54@gmail.com>
2020-12-27 17:00:16 +0300

    .

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.colormyviews
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -33,13 +34,15 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
                 listOf(box_one_text, box_two_text, box_three_text,
-                        box_four_text, box_five_text,constraint_layout)
+                        box_four_text, box_five_text, constraint_layout,
+                        red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun makeColored(view: View) {
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
@@ -47,6 +50,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
