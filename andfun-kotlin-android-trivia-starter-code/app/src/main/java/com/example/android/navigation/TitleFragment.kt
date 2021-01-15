2e8ed8d4769356d6b84e1be720fa11d2b58e45e3
yunuskiran <yunuskiran54@gmail.com>
2021-01-15 21:18:26 +0300

    .

---
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,34 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.fragment_title.*
+
+class TitleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_title, container, false)
+        val btn: Button = view.findViewById(R.id.playButton)
+        btn.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment2_to_gameFragment)
+        )
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+                view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
