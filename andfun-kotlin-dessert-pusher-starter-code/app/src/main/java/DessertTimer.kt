0ccbc415cf604d1c6486939b23643aec0946e45e
yunuskiran <yunuskiran54@gmail.com>
2021-01-18 16:27:59 +0300

    guess it project added

---
+++ andfun-kotlin-dessert-pusher-starter-code/app/src/main/java/DessertTimer.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.dessertpusher
+
+import android.os.Handler
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import timber.log.Timber
+
+/**
+ * This is a class representing a timer that you can start or stop. The secondsCount outputs a count of
+ * how many seconds since it started, every one second.
+ *
+ * -----
+ *
+ * Handler and Runnable are beyond the scope of this lesson. This is in part because they deal with
+ * threading, which is a complex topic that will be covered in a later lesson.
+ *
+ * If you want to learn more now, you can take a look on the Android Developer documentation on
+ * threading:
+ *
+ * https:developer.android.com/guide/components/processes-and-threads
+ *
+ */
+class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
+
+    //The number of seconds counted since the timer started
+    var secondsCount = 0
+
+    private var handler = Handler()
+    private lateinit var runnable: Runnable
+
+    init {
+        lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun startTimer() {
+        runnable = Runnable {
+            secondsCount++
+            Timber.i("Timer is at : $secondsCount")
+            handler.postDelayed(runnable, 1000)
+        }
+
+        //  This is what initially starts the timer
+        handler.postDelayed(runnable, 1000)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun stopTimer() {
+        handler.removeCallbacks(runnable)
+    }
+}
